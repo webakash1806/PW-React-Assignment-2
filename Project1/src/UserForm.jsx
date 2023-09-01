65f275ad0025703aea630aa99dafc7709cf70b7f
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-01 19:44:22 +0530

    UserForm functionality added

--- Project1/src/UserForm.jsx
+++ Project1/src/UserForm.jsx
@@ -1,16 +1,22 @@
-const UserForm = () => {
+import { useState } from "react"
 
+const UserForm = () => {
+    const [login, setLogin] = useState(false)
+
+    const loginCheck = () => {
+        setLogin(!login)
+    }
 
     return (
         <>
             <div className="container">
                 <h1>Instagram</h1>
                 <input type="text" placeholder="Mobile No. or Email" />
-                <input type="text" name="" id="" placeholder="Full Name" />
+                <input type="text" name="" id="" placeholder="Full Name" hidden={login} />
                 <input type="password" name="" id="" placeholder="Password" />
-                <input type="password" name="" id="" placeholder="Confirm-Password" />
-                <button>Sign Up</button>
-                <p>Already have an account? <span>Login</span></p>
+                <input type="password" name="" id="" placeholder="Confirm-Password" hidden={login} />
+                <button>{login ? "login" : "Sign up"}</button>
+                <p>{login ? "Don't have an account?" : "Already have an account?"} <span onClick={loginCheck}>{login ? "Sign up" : "Login"}</span></p>
             </div>
         </>
     )
