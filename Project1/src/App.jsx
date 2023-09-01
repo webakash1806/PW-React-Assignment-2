5f530eef0184b001f670aaa437d9e07a15c7e1c9
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-01 18:56:34 +0530

    User form created

---
+++ Project1/src/App.jsx
@@ -0,0 +1,11 @@
+import UserForm from "./UserForm";
+
+const App = () => {
+    return (
+        <>
+            <UserForm />
+        </>
+    )
+}
+
+export default App
---
+++ Project1/src/UserForm.jsx
@@ -0,0 +1,20 @@
+const UserForm = () => {
+
+
+    return (
+        <>
+            <div className="container">
+                <h1>Instagram</h1>
+                <h3>Sign Up</h3>
+                <input type="text" placeholder="Mobile No. or Email" />
+                <input type="text" name="" id="" placeholder="Full Name" />
+                <input type="password" name="" id="" placeholder="Password" />
+                <input type="password" name="" id="" placeholder="Confirm-Password" />
+                <button>Sign Up</button>
+                <p>Already have an account? <span>Login</span></p>
+            </div>
+        </>
+    )
+}
+
+export default UserForm
