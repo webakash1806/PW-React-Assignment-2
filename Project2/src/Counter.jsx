df9d3c470131887fc38cfa571ea79e89f6a1d60e
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-01 16:46:41 +0530

    Counter file updated

--- Project2/src/Counter.jsx
+++ Project2/src/Counter.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 const Counter = () => {
-    const [count, setCount] = useState(0)
+    let [count, setCount] = useState(0)
     return (
         <>
             <p className="result">{count}</p>
